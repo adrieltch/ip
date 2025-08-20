7d89e082dd00a9cedabe11a996393c6cc563cf06
adrieltch <e1398590@u.nus.edu>
2025-08-20 17:53:29 +0800

    Level-1, changed level-0 to use OOP principles

--- src/main/java/fatty.java
+++ src/main/java/fatty.java
@@ -1,11 +1,43 @@
+import java.util.*;
+
 public class fatty {
+    private static final String horizontalLine = "_".repeat(75);
+
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________\n" +
-                "Hello! I'm fatty\n" + "What can I do for you?\n"
-                +"____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n");
+        fatty.greet();
+        fatty.echo();
     }
+
+    private static void greet() {
+        System.out.println( horizontalLine + "\n"
+                + "Hello! I'm fatty.\n"
+                + "What can I do for you?\n"
+                + horizontalLine);
+    }
+
+    private static void echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                fatty.bye();
+                scanner.close();
+                return;
+            } else {
+                System.out.println(horizontalLine + "\n" +
+                        input + "\n" +
+                        horizontalLine);
+            }
+
+        }
+    }
+
+    private static void bye(){
+        System.out.println(horizontalLine + "\n"
+                + "Bye. Hope to see you again soon!\n"
+                + horizontalLine);
+    }
+
 }
 
 
