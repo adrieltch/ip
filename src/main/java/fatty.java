1cb7f9a84c7ea829f942571506f939b4fe4dbf5f
adrieltch <e1398590@u.nus.edu>
2025-08-21 01:04:06 +0800

    Level-2, removed static modifier, renamed to Fatty

--- src/main/java/fatty.java
+++ src/main/java/fatty.java
@@ -1,41 +1,65 @@
 import java.util.*;
 
-public class fatty {
+public class Fatty {
     private static final String horizontalLine = "_".repeat(75);
 
+    private String list = "";
+    private int count = 0;
+
     public static void main(String[] args) {
+        Fatty fatty = new Fatty();
         fatty.greet();
         fatty.echo();
     }
 
-    private static void greet() {
+    private void greet() {
         System.out.println( horizontalLine + "\n"
                 + "Hello! I'm fatty.\n"
                 + "What can I do for you?\n"
-                + horizontalLine);
+                + horizontalLine );
     }
 
-    private static void echo() {
+    private void echo() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
-            if (input.equals("bye")) {
-                fatty.bye();
-                scanner.close();
-                return;
-            } else {
-                System.out.println(horizontalLine + "\n" +
-                        input + "\n" +
-                        horizontalLine);
-            }
 
+            switch (input) {
+                case "bye":
+                    bye();
+                    scanner.close();
+                    return;
+
+                case "list":
+                    getList();
+                    break;
+
+                default:
+                    addList(input);
+                    System.out.println(horizontalLine + "\n" +
+                            "added: " + input + "\n" +
+                            horizontalLine);
+                    break;
+            }
         }
+
     }
 
-    private static void bye(){
+    private void bye(){
         System.out.println(horizontalLine + "\n"
                 + "Bye. Hope to see you again soon!\n"
                 + horizontalLine);
+    }
+
+    private void getList(){
+        System.out.println(horizontalLine + "\n"
+        + this.list + horizontalLine);
+    }
+
+    private void addList(String input) {
+        this.count++;
+        String temp = String.valueOf(this.count);
+        this.list += temp + ". " + input + "\n";
     }
 
 }
